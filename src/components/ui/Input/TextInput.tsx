01f5e19191ccff05e4b2f64c49bc99eee1a79596
MarcosSantosLocX <marcos.santos@locximoveis.com.br>
2025-02-03 23:10:48 -0300

    feat: TextInput component

--- src/components/ui/Input/TextInput.tsx
+++ src/components/ui/Input/TextInput.tsx
@@ -2,32 +2,36 @@ import React from 'react';
 
 import { TextField, FormControl, FormHelperText } from '@mui/material';
 
-import { Controller, FieldValues, Path, UseControllerProps } from 'react-hook-form';
+import { Controller, FieldError, FieldErrorsImpl, FieldValues, Merge, Path, UseControllerProps } from 'react-hook-form';
 
 type ControlledProps<T extends FieldValues> = {
 	control: UseControllerProps<T>['control'];
 	name: Path<T>;
 	rules?: UseControllerProps<T>['rules'];
+	helperText?: string | FieldError | Merge<FieldError, FieldErrorsImpl<T>>;
 };
 
 type UncontrolledProps = {
 	value?: string;
 	defaultValue?: string;
 	onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	helperText?: string | FieldError | Merge<FieldError, FieldErrorsImpl<any>>;
 };
 
 // Props comuns para ambos os componentes
 type CommonProps = {
+	name: string;
 	label: string;
 	type?: string;
 	placeholder?: string;
-	helperText?: string;
+	disabled?: boolean;
 	error?: boolean;
 	fullWidth?: boolean;
-	ref: React.Ref<HTMLInputElement>;
-	name: string;
-	onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-	onBlur: (event: React.FocusEvent<HTMLInputElement>) => void;
+	value?: string;
+	ref?: React.Ref<HTMLInputElement>;
+	onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+	onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
 };
 
 const ControlledInput = <T extends FieldValues>({
@@ -36,6 +40,7 @@ const ControlledInput = <T extends FieldValues>({
 	helperText,
 	error,
 	fullWidth = true,
+	label,
 	...props
 }: ControlledProps<T> & CommonProps) => {
 	return (
@@ -52,10 +57,12 @@ const ControlledInput = <T extends FieldValues>({
 						{...field}
 						type="text"
 						error={error}
+						label={label}
+						inputRef={field.ref}
 					/>
 				)}
 			/>
-			{helperText && <FormHelperText>{helperText}</FormHelperText>}
+			{typeof helperText === 'string' && helperText && <FormHelperText>{helperText}</FormHelperText>}
 		</FormControl>
 	);
 };
@@ -72,7 +79,7 @@ const UncontrolledInput = React.forwardRef<HTMLInputElement, UncontrolledProps &
 					type="text"
 					inputRef={ref}
 				/>
-				{helperText && <FormHelperText>{helperText}</FormHelperText>}
+				{typeof helperText === 'string' && helperText && <FormHelperText>{helperText}</FormHelperText>}
 			</FormControl>
 		);
 	}
